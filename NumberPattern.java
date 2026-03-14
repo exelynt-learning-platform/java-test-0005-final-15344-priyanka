31424db6f39e54a208dfe0963c31d3e3f7365a46
priyanka-gawali <gawaligun@gmail.com>
2026-03-14 15:51:22 +0530

    Add Java program to print Number patterns

--- NumberPattern.java
+++ NumberPattern.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class NumberPattern {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter number of rows: ");
         int totalRows = sc.nextInt();
 
         printPattern(totalRows);
+
+        sc.close(); 
     }
 
     public static void printPattern(int totalRows) {
@@ -16,10 +18,7 @@ public class NumberPattern {
 
         for (int i = 1; i <= totalRows; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(num);
-                if (j < i) {
-                    System.out.print(" ");
-                }
+                System.out.print(num + " ");
                 num++;
             }
             System.out.println();
