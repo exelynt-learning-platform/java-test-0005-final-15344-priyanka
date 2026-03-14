f4613e60400e1f60974ef0cd657604b810a4de37
priyanka-gawali <gawaligun@gmail.com>
2026-03-14 15:43:42 +0530

    Add Java program to print Number pattern

--- NumberPattern.java
+++ NumberPattern.java
@@ -1,19 +1,19 @@
 public class NumberPattern {
 
-    private static final int ROWS = 5;
+    private static final int TOTAL_ROWS = 5;
 
-public static void main(String[] args) {
-    int num = 1;
+    public static void main(String[] args) {
+        int num = 1;
 
-    for (int i = 1; i <= ROWS; i++) {
-        for (int j = 1; j <= i; j++) {
-            System.out.print(num);
-            if (j < i) {
-                System.out.print(" ");
+        for (int i = 1; i <= TOTAL_ROWS; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num);
+                if (j < i) {
+                    System.out.print(" ");
+                }
+                num++;
             }
-            num++;
+            System.out.println();
         }
-        System.out.println();
     }
-}
 }
